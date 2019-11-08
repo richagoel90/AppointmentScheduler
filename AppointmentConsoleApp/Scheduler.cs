4ed1adc47c081c799057a5b0de53c3500b145aed
Richa Goel <richa.goyal90@gmail.com>
2019-11-07 16:43:52 -0800

    Entity framework

--- AppointmentConsoleApp/Scheduler.cs
+++ AppointmentConsoleApp/Scheduler.cs
@@ -9,16 +9,6 @@ namespace AppointmentConsoleApp
     {
         private static AppointmentContext db = new AppointmentContext();
         
-        //public static void UserList_Initializer()
-        //{
-        //    db.Users.Add(new UserInfo() {FirstName = "Richa", LastName = "Goel", EmailID = "richa.goyal90@gmail.com", PhoneNumber = 4254432538, UserName = "RichaGoel", Password = "richa" });
-        //    db.Users.Add(new UserInfo() {FirstName = "Arpit", LastName = "Gupta", EmailID = "arpit.gupta@gmail.com", PhoneNumber = 4254432222, UserName = "ArpitGupta", Password = "arpit" });
-        //    db.Users.Add(new UserInfo() {FirstName = "Deepak", LastName = "Goel", EmailID = "deepak.goel@gmail.com", PhoneNumber = 4254432333, UserName = "DeepakGoel", Password = "deepak" });
-        //    db.Users.Add(new UserInfo() {FirstName = "Saurabh", LastName = "Goel", EmailID = "saurabh.goel@gmail.com", PhoneNumber = 4254434444, UserName = "SaurabhGoel", Password = "saurabh" });
-        //    db.Users.Add(new UserInfo() {FirstName = "Bhawna", LastName = "Goel", EmailID = "bhawna.goel@gmail.com", PhoneNumber = 4254435555, UserName = "BhawnaGoel", Password = "bhawna" });
-        //    db.Users.Add(new UserInfo() {FirstName = "Jyoti", LastName = "Goel", EmailID = "jyoti.goel@gmail.com", PhoneNumber = 4254435556, UserName = "JyotiGoel", Password = "jyoti" });
-        //    db.SaveChanges();
-        //}
         public static IEnumerable<UserInfo> getGuestUserList( int UserId)
         {
             return db.Users.Where(a => a.UserId != UserId);
